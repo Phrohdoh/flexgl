2cca3d3d80ec803f4b8d0804a26c5f34aea0c216
Taryn Hill <taryn@phrohdoh.com>
2017-09-20 18:46:50 -0500

    typedef flex_item in ui

--- src/ui.c
+++ src/ui.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "../includes/flex.h"
 
+// I don't want to type 'struct' over and over.
+typedef struct flex_item flex_item;
+
 void ui_do_thing(void) {
-    struct flex_item *itm;
+    flex_item *itm;
     itm = flex_item_new();
     printf("%p\n", itm);
     flex_item_free(itm);
