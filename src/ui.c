ed12fe6a599a183ddb90158c4ca20318111404ed
Taryn Hill <taryn@phrohdoh.com>
2017-09-20 19:04:08 -0500

    write, read, and print 'itm' width and height

--- src/ui.c
+++ src/ui.c
@@ -7,6 +7,16 @@ typedef struct flex_item flex_item;
 void ui_do_thing(void) {
     flex_item *itm;
     itm = flex_item_new();
-    printf("%p\n", itm);
+
+    flex_item_set_width(itm, 30.0f);
+    flex_item_set_height(itm, 50.0f);
+
+    float w = flex_item_get_width(itm);
+    float h = flex_item_get_height(itm);
+
+    flex_layout(itm);
+
+    printf("%0.2f x %0.2f\n", w, h);
+
     flex_item_free(itm);
 }
